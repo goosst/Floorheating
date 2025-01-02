44a2610ef6f1baaefa93f46490c8e67f0e0f3f8e
goosst <goosst@gmail.com>
2025-01-02 19:45:02 +0100

    extension for shell command

--- Octave/Runtime/PostHomeAssistant.m
+++ Octave/Runtime/PostHomeAssistant.m
@@ -2,17 +2,20 @@ function [entity_data,hass_time,most_recent_states]=PostHomeAssistant(address_ha
 % I could not get it to work using webwrite in octav, so used curl and some system command
 
 
-typeofentity = strfind(entity_id, 'switch');
+typeiswitch = strfind(entity_id, 'switch.');
+typeisshellcommand = strfind(entity_id, 'shell_command.');
 
-if ~isempty(typeofentity)
+if ~isempty(typeiswitch)
 
           % Determine which API call to make based on the value
-        if value >= (1-eps)
+        if value >= 0.9
             service = 'turn_on';
-        elseif value <= eps
+        elseif value <= 1e-4
             service = 'turn_off';
         else
+            display(value)
             error('Invalid value for switch. Use 1 to turn on or 0 to turn off.');
+
         end
 
         % Construct the API endpoint for switch services
@@ -21,7 +24,7 @@ if ~isempty(typeofentity)
 
 
         % Construct the curl command to call the API
-        curl_cmd = sprintf(['curl "%s" ' ...
+        curl_cmd = sprintf(['curl -s "%s" ' ...
                             '-H "Authorization: Bearer %s" ' ...
                             '-H "Content-Type: application/json" ' ...
                             '-d ''{"entity_id": "%s"}'' '], ...
@@ -30,7 +33,17 @@ if ~isempty(typeofentity)
         % Execute the curl command
         [status, response] = system(curl_cmd);
 
+elseif ~isempty(typeisshellcommand)
 
+  commandedshell=strsplit(entity_id, '.');
+  api_endpoint = sprintf('http://%s/api/services/shell_command/%s', address_hass, commandedshell{2});
+  curl_cmd = sprintf(['curl -s -X POST --max-time %d "%s" ' ...
+                    '-H "Authorization: Bearer %s" ' ...
+                    '-H "Content-Type: application/json" ' ...
+                    '-d "{}"'], ...
+                    58,api_endpoint, auth_token);
+
+  [status, response] = system(curl_cmd)
 
 else
 
